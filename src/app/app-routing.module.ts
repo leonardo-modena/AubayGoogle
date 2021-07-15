9ab45404db1e11bec8e83b1488e920ff88bc82f6
leomod272 <leomod272@gmail.com>
2021-07-15 11:17:33 +0200

    set 2 route primarie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './component/admin/admin.component';
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: SearchBarComponent },
+  { path: "auth", component: AdminComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
