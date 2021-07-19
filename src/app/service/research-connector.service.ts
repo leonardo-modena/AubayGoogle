85ac04e68d7c6c541f29e7558ccbdc4db3dc4161
leomod272 <leomod272@gmail.com>
2021-07-19 16:25:21 +0200

    link paginazione visibili tramine conosle.log

--- src/app/service/research-connector.service.ts
+++ src/app/service/research-connector.service.ts
@@ -1,52 +1,58 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Research} from '../model/research.model';
-import {map} from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Research } from '../model/research.model';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResearchConnectorService {
-  constructor(
-    private httpService: HttpClient,
-  ) {
-  }
+  constructor(private httpService: HttpClient) {}
 
-  research(chiave: string): Observable<Research[]> {
-    return this.httpService.get<Research[]>(
-      `http://localhost:3000/ricerca?q=${chiave}&_limit=1&_page=1`
-    ).pipe(map((response) => {
-      console.log(response);
-      response.forEach((item: any) => {
-        if (item.nuovaRicerca) {
-          item["titolo"] = item.nuovaRicerca.titolo;
-          item["descrizione"] = item.nuovaRicerca.descrizione;
-          item["chiavi"] = item.nuovaRicerca.chiavi;
-          item["url"] = item.nuovaRicerca.url;
-        }
-      })
-      return response;
-    }));
+  research(chiave: string) {
+    return this.httpService
+      .get<Research[]>(
+        `http://localhost:3000/ricerca?q=${chiave}&_limit=1&_page=1`,
+        { observe: 'response' }
+      )
+      .pipe(
+        map((response) => {
+          let resData: any[] = [];
+          if (response.body) {
+            resData = response.body;
+          }
+          resData.forEach((item: any) => {
+            if (item.nuovaRicerca) {
+              item['titolo'] = item.nuovaRicerca.titolo;
+              item['descrizione'] = item.nuovaRicerca.descrizione;
+              item['chiavi'] = item.nuovaRicerca.chiavi;
+              item['url'] = item.nuovaRicerca.url;
+            }
+          });
+
+          console.log(response.headers.get("Link"))
+
+          return resData;
+        })
+      );
   }
 
   newResearch(nuovaRicerca: Research) {
     let token;
-    token = localStorage.getItem("user")
+    token = localStorage.getItem('user');
 
     if (token != null) {
-      token = JSON.parse(token)
+      token = JSON.parse(token);
       token = token.access_token;
-      console.log(token)
-    } else
-      return;
+      console.log(token);
+    } else return;
 
-    return this.httpService
-      .post<Research>(
-        'http://localhost:3000/ricerca',
-        {nuovaRicerca},
-        {headers: {Authorization: `Bearer ${token}`}}
-      );
+    return this.httpService.post<Research>(
+      'http://localhost:3000/ricerca',
+      { nuovaRicerca },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
   }
 }
